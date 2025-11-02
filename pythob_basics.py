166c14cd21751c10abe3699e00195e23cffe088e
Prabhudeep Sharma <sharmaprabhu761@gmail.com>
2025-11-02 17:25:02 +0530

    first file

---
+++ pythob_basics.py
@@ -0,0 +1,21 @@
+print("hekko worlfd")
+print("prabhu " + "sharma")
+spam = 40
+eggs = 2
+
+print(spam+eggs)
+
+#this program says hello and ask my name
+
+print("what is your name?")
+message = input("please tell me your name")
+print("My name is "+message)
+
+type(message)
+
+a =int(1)
+b = int(5)
+c = float(10)
+round(3.14)
+
+print(a+b)
