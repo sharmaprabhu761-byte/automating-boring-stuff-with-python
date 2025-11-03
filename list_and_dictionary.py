be53ceb075ec692f844547b027d1aa7c73652a99
Prabhudeep Sharma <sharmaprabhu761@gmail.com>
2025-11-03 20:33:48 +0530

    adding

---
+++ list_and_dictionary.py
@@ -0,0 +1,12 @@
+cat = {'size':'fat','color':'gray','age':10}
+
+print(cat)
+
+print(cat['size'])
+
+
+for v in cat.values():
+    print(v)
+
+for v in cat.keys():
+    print(v)
---
+++ strings.py
@@ -0,0 +1,3 @@
+spam = "that is alice cat"
+spam = 'this is my  cat\'s purse'
+print(spam)
